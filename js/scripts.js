0961d39006e6701009a725228c2bbb964809aba8
Joseph Tubridy <jftubridy@gmail.com>
2019-06-07 09:40:00 -0700

    add age control to display survey, fix syntax so that survey shows up

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,20 @@
 
 
 
-$() {
+$(function(){
+  var age;
 
+  $("#age").submit(function(event){
+    age = parseInt($("input#age").val());
 
-
-
-
+    if (age <8){
+      $(".form1").hide();
+      $(".lang").hide();
+      alert("Maybe enjoy childhood a little longer");
+    } else {
+      $(".form1").show();
+      $(".lang").show();
+    }
+    event.preventDefault();
+  })
 });
