63974dbacae1bddb4d38a432a1c6f99fa7fa9e4a
Joseph Tubridy <jftubridy@gmail.com>
2019-06-07 11:46:41 -0700

    change age input from 'text' to 'number' to not accept strings

--- js/scripts.js
+++ js/scripts.js
@@ -27,60 +27,60 @@ $(function(){
     event.preventDefault();
   })
 
-  // $(".surv").submit(function(event){
-  //   lvl = $("#lvl").val();
-  //   obj = $("#obj").val();
-  //   eg = $("#eg").val();
-  //
-  //   if (eg == 3) {
-  //     $("#pol").show();
-  //     $("#htm").hide();
-  //     $("#cs").hide();
-  //     $("#jsrpt").hide();
-  //   } else if (lvl ==1 && obj ==1){
-  //     $("#htm").show();
-  //     $("#cs").hide();
-  //     $("#jsrpt").hide();
-  //     $("#pol").hide();
-  //   } else if (lvl ==2 && obj ==2){
-  //     $("#htm").hide();
-  //     $("#cs").show();
-  //     $("#jsrpt").hide();
-  //     $("#pol").hide();
-  //   } else {
-  //     $("#htm").hide();
-  //     $("#cs").hide();
-  //     $("#jsrpt").show();
-  //     $("#pol").hide();
-  //   }
-
-
   $(".surv").submit(function(event){
     lvl = $("#lvl").val();
     obj = $("#obj").val();
     eg = $("#eg").val();
 
     if (eg == 3) {
-      $("#pol").addClass("show");
-      $("#htm").removeClass("show");
-      $("#cs").removeClass("show");
-      $("#jsrpt").removeClass("show");
-    } else if (lvl ==1 && obj ==1){
-      $("#htm").addClass("show");
-      $("#cs").removeClass("show");
-      $("#jsrpt").removeClass("show");
-      $("#pol").removeClass("show");
-    } else if (lvl ==2 && obj ==2){
-      $("#htm").removeClass("show");
-      $("#cs").addClass("show");
-      $("#jsrpt").removeClass("show");
-      $("#pol").removeClass("show");
+      $("#pol").show();
+      $("#htm").hide();
+      $("#cs").hide();
+      $("#jsrpt").hide();
+    } else if (lvl ==1, 2 && obj ==1){
+      $("#htm").show();
+      $("#cs").hide();
+      $("#jsrpt").hide();
+      $("#pol").hide();
+    } else if (lvl ==2, 3 && obj ==2){
+      $("#htm").hide();
+      $("#cs").show();
+      $("#jsrpt").hide();
+      $("#pol").hide();
     } else {
-      $("#htm").removeClass("show");
-      $("#cs").removeClass("show");
-      $("#jsrpt").addClass("show");
-      $("#pol").removeClass("show");
+      $("#htm").hide();
+      $("#cs").hide();
+      $("#jsrpt").show();
+      $("#pol").hide();
     }
+
+
+  // $(".surv").submit(function(event){
+  //   lvl = $("#lvl").val();
+  //   obj = $("#obj").val();
+  //   eg = $("#eg").val();
+  //
+  //   if (eg == 3) {
+  //     $("#pol").addClass("show");
+  //     $("#htm").removeClass("show");
+  //     $("#cs").removeClass("show");
+  //     $("#jsrpt").removeClass("show");
+  //   } else if (lvl ==1 && obj ==1){
+  //     $("#htm").addClass("show");
+  //     $("#cs").removeClass("show");
+  //     $("#jsrpt").removeClass("show");
+  //     $("#pol").removeClass("show");
+  //   } else if (lvl ==2 && obj ==2){
+  //     $("#htm").removeClass("show");
+  //     $("#cs").addClass("show");
+  //     $("#jsrpt").removeClass("show");
+  //     $("#pol").removeClass("show");
+  //   } else {
+  //     $("#htm").removeClass("show");
+  //     $("#cs").removeClass("show");
+  //     $("#jsrpt").addClass("show");
+  //     $("#pol").removeClass("show");
+  //   }
 
     event.preventDefault();
   });
